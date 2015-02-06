84733733778825e9a25359948a81bc3f77ea4036
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-02-06 23:04:27 +0100

    update plugin; move voice from build script into custom config file

--- voice.groovy
+++ voice.groovy
@@ -1,5 +1,13 @@
-license {
-    name = 'Creative Commons Attribution-ShareAlike 3.0 Unported'
-    shortName = 'CC-BY-SA'
-    url = 'http://mary.dfki.de/download/by-sa-3.0.html'
+voice {
+    name = 'upmc-jessica-hsmm'
+    language = 'fr'
+    gender = 'female'
+    type = 'hsmm'
+    description = 'A female French hidden semi-Markov model voice, built at ISIR (UPMC) using data recorded by Jessica Durand'
+    license {
+        name = 'Creative Commons Attribution-ShareAlike 3.0 Unported'
+        shortName = 'CC-BY-SA'
+        url = 'http://mary.dfki.de/download/by-sa-3.0.html'
+    }
+    samplingRate = 16000
 }
